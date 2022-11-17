e68b6d7ed8a20f0437798ca070bc49ccefa1a6f3
Alex Riss <00alexx@riss.at>
2022-11-17 21:47:42 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,14 @@ using Test
     res = @capture_out install(;test_run=true, interactive=false)
     
     @test contains(res, "beverage")
+
+    @test contains(res, "Start Menu")
+    @test contains(res, "Setting up.")
+    @test contains(res, "Getting package.")
+    @test contains(res, "Bundling.")
+    @test contains(res, "Compiling.")
+
     @test contains(res, "complete")
     @test contains(res, "Enjoy")
+
 end
